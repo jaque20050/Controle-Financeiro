58eca221a587ac6fa69c56e8ae03424b352079fb
jaque20050 <jaque20050@gmail.com>
2022-02-01 17:14:53 -0300

    canculando o valor total do saldo

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const transactionsUl = document.querySelector("#transactions");
+const incomeDisplay = document.querySelector("#money-plus");
+const expenseDisplay = document.querySelector("#money-minus");
+const balanceDisplay = document.querySelector("#balance");
+
+
 
 const dummyTransactions = [
   { id: 1, name: "Bolo de brigadeiro", amount: -20 },
@@ -21,13 +26,30 @@ const addTransactionIntoDOM = (transaction) => {
 };
 
 const updateBalanceValues = () => {
-    const transactionAmounts = dummyTransactions.map(transaction => transaction.amount)
-    console.log(transactionAmounts)
-}
+  const transactionAmounts = dummyTransactions.map(
+    (transaction) => transaction.amount
+  );
+  const total = transactionAmounts
+    .reduce((accumulator, transaction) => accumulator + transaction, 0)
+    .toFixed(2);
+  const income = transactionAmounts
+    .filter((value) => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+    .toFixed(2);
+  const expense = Math.abs(transactionAmounts
+    .filter((value) => value < 0)
+    .reduce((accumulator, value) => accumulator + value, 0))
+    .toFixed(2);
+  
+    balanceDisplay.textContent = `R$ ${ total }`
+    incomeDisplay.textContent = `R$ ${ income }`
+    expenseDisplay.textContent = `R$ ${ expense }`
+
+};
 
 const init = () => {
   dummyTransactions.forEach(addTransactionIntoDOM);
-  updateBalanceValues()
+  updateBalanceValues();
 };
 
-init()
+init();
